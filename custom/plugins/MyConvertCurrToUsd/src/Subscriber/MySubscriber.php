8e7dfc16b83a155bb1ac69939a7f24510dc539b5
fantasyz <testphp72019@gmail.com>
2021-04-07 02:02:01 +0300

    add custom factor to usd currency plugin

--- custom/plugins/MyConvertCurrToUsd/src/Subscriber/MySubscriber.php
+++ custom/plugins/MyConvertCurrToUsd/src/Subscriber/MySubscriber.php
@@ -9,18 +9,20 @@ use Shopware\Core\Content\Product\ProductEvents;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
 use Shopware\Core\Framework\Struct\ArrayEntity;
-use Shopware\Storefront\Pagelet\Header\HeaderPageletLoadedEvent;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 
 class MySubscriber implements EventSubscriberInterface
 {
     private $convert;
+    private $systemConfigService;
 
 
-    public function __construct(ConvertService $convert)
+    public function __construct(ConvertService $convert, SystemConfigService $systemConfigService)
     {
         $this->convert = $convert;
+        $this->systemConfigService = $systemConfigService;
     }
 
     public static function getSubscribedEvents(): array
@@ -33,10 +35,12 @@ class MySubscriber implements EventSubscriberInterface
     public function onProductsLoaded(EntityLoadedEvent $event)
     {
         $currencyFactorUsd = $this->convert->getCurrencyFactor('USD');
+        $customCurrencyFactorUsd = $this->systemConfigService->get('MyConvertCurrToUsd.config.customCurrencyFactorUsd');
+        $currencyFactor = $customCurrencyFactorUsd ? $customCurrencyFactorUsd : $currencyFactorUsd;
 
         foreach ($event->getEntities() as $item) {
             $currencyEU = $item->getCurrencyPrice(Defaults::CURRENCY)->getGross();
-            $priceUsd = $currencyFactorUsd * $currencyEU;
+            $priceUsd = $currencyFactor * $currencyEU;
 
             $item->addExtension('currency_usd',new ArrayEntity(['currency_usd'=> $priceUsd, 'standart_currency'=> $currencyEU]));
         }
